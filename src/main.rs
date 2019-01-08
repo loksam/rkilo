c46d82b89d82fb81c60f76872b1fdbe7d5a9be2a
loksam <loksam.jp@gmail.com>
2019-01-08 21:52:51 +0900

    Add quit for loop.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 use std::io::{Write, stdout, stdin};
+use std::process;
 
 fn main() {
 
     loop {
         let mut s = String::new();
         std::io::stdin().read_line(&mut s).ok();
+        s.pop();
+
+        match &*s {
+            "q" => process::exit(1),
+            _ => continue,
+        }
+
     }
-    
+
 }
