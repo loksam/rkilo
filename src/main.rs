1771e377e5141f52dd43e692ec269a8618cbfda0
loksam <loksam.jp@gmail.com>
2019-01-09 00:21:57 +0900

    Disable raw mode at exit.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,37 @@
-use std::io::{Write, stdout, stdin};
+extern crate termios;
+
 use std::process;
+use std::io;
+use std::io::Read;
+use std::io::Write;
+use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
+use std::str;
 
 fn main() {
 
-    loop {
-        let mut s = String::new();
-        std::io::stdin().read_line(&mut s).ok();
-        s.pop();
+    let stdin = 0; 
+    let termios = Termios::from_fd(stdin).unwrap();
+    let mut new_termios = termios.clone(); 
+    new_termios.c_lflag &= !(ICANON | ECHO);
+    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+    let stdout = io::stdout();
+    let mut reader = io::stdin();
+    let mut buffer = [0;1];
 
-        match &*s {
-            "q" => process::exit(1),
+    loop {
+        stdout.lock().flush().unwrap();
+        reader.read_exact(&mut buffer).unwrap();
+
+        let s = str::from_utf8(&buffer).unwrap();
+        println!("{:?}{}", buffer, s);
+
+        match s {
+            "q" => {
+                tcsetattr(stdin, TCSANOW, & termios).unwrap(); 
+                process::exit(1)
+            }
             _ => continue,
         }
-
-    }
+    } 
 
 }
